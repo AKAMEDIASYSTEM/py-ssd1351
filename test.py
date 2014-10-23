25c6362f3dd6cc9d8a6e36b41b0875686f3e3089
Noah Feehan <nfeehan@gmail.com>
2014-10-23 12:00:56 -0400

    color testing

--- test.py
+++ test.py
@@ -46,13 +46,13 @@ while True:
 
     disp.clear_display()
     # disp.draw_text(32,32,'hey',disp.color565(0,0,255)) # should be blue
-    disp.draw_text(32,32,'hey',0xFFFFFF) # should be green
+    disp.draw_text(32,32,'hey',0x00FF00) # should be green
     # disp.dump_buffer()
     # disp.data(disp.bitmap)
     time.sleep(1)
 
     disp.clear_display()
-    disp.fillScreen(0xFF0000)
+    disp.fillScreen(0x0000FF)
     # disp.fillScreen(disp.color565(0,255,0)) # should be green
     time.sleep(0.5)
 
