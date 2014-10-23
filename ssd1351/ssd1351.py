cfc0204bff6fd1c5aec1bc3ab3b6ee5fba12a394
Noah Feehan <nfeehan@gmail.com>
2014-10-23 13:18:18 -0400

    color testing

--- ssd1351/ssd1351.py
+++ ssd1351/ssd1351.py
@@ -199,7 +199,7 @@ class SSD1351:
         return c
 
     def fillScreen(self, fillcolor): # int
-        self.fillRect(0, 0, self.SSD1351WIDTH, self.SSD1351HEIGHT, self.encode_color(fillcolor))
+        self.fillRect(0, 0, self.SSD1351WIDTH, self.SSD1351HEIGHT, fillcolor)
 
     def fillRect(self, x, y, w, h, fillcolor):
         # Bounds check
