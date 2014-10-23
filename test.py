6da826e372bdf5cb964159e3a3b3737dcec8d729
Noah Feehan <nfeehan@gmail.com>
2014-10-23 11:44:27 -0400

    color testing

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ while True:
     time.sleep(1)
 
     disp.clear_display()
-    disp.fillScreen(0x00FF00)
+    disp.fillScreen(0xFF00FF)
     # disp.fillScreen(disp.color565(0,255,0)) # should be green
     time.sleep(0.5)
 
