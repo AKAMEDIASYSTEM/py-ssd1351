8550b8e5258f5a6906f3a55c29fbfc7b9c359c5e
Noah Feehan <nfeehan@gmail.com>
2014-10-23 16:31:22 -0400

    trying to improve font readability

--- ssd1351/ssd1351.py
+++ ssd1351/ssd1351.py
@@ -281,7 +281,6 @@ class SSD1351:
                         # self.bitmap.draw_pixel(x, y+row, 0)
                     mask >>= 1
                 x += 1
-            x += 5 # space between characters
 
     def draw_text_bg(self, x, y, string, color=0xFFFFFF, bg=0x000000):
         # print 'text is %s' % string
